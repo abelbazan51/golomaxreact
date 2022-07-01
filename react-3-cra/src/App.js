9582612feccf324ae1d46095a05483c804888e47
abel bazan <abelbazan51@gmail.com>
2022-06-30 21:06:48 -0300

    reacct

---
+++ react-3-cra/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">bienvenidos a golomax</span>
+        </div>
+      </nav>
+      <header className="App-header">hola</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-golo/react-3-cra/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+            <h5 class="text-white h4">Collapsed content</h5>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+          <div class="container-fluid">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </nav>
+        hola mundo
+      </header>
+    </div>
+  );
+}
+
+export default App;
